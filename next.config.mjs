23862b885cab500bcb0986f573cfeae6abd76b8c
Eze Chinedu <davidchinedu310@gmail.com>
2024-10-31 23:10:44 +0100

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,30 @@
+ /** @type {import('next').NextConfig} */
+const nextConfig = {
+images: {
+    domains: [
+        "res.cloudinary.com"
+    ]
+}
+};
+
+export default nextConfig;
+
+
+// next.config.mjs
+// export default {
+//     images: {
+//       remotePatterns: [
+//         {
+//           protocol: 'https',
+//           hostname: 'res.cloudinary.com',
+//           port: '',
+//                 pathname: '/v1712195254/**', // Adjust the pathname as needed
+//         },
+//       ],
+//     },
+//   };
+  
+
+
+  
+
